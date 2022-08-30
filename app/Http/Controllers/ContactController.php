9f9aef5001322c948b707e444a87ab10d5b54140
Dexter Ladines <ladines.dexter@nexseed.net>
2022-08-30 13:16:22 +0800

    done with contact-us form with send-email

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+
+class ContactController extends Controller
+{
+    public function index() 
+    {
+        return view('contactForm');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|digits:11|numeric',
+            'subject' => 'required',
+            'message' => 'required'
+        ]);
+
+        Contact::create($request->all());
+
+        return redirect() ->back()
+                         ->with(['success' => 'Thank you for contacting us. We will contact you soon.']);
+    }
+}
